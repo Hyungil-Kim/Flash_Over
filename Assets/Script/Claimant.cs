a6c70eccf69a4ce68e6e890bb93f03cddf9db67e
LarJi <87188826+LarJi@users.noreply.github.com>
2022-01-07 12:37:13 +0900

    ??????

--- Assets/Script/Claimant.cs
+++ Assets/Script/Claimant.cs
@@ -79,11 +79,11 @@ public class Claimant : FSM<ClaimantState>
 	public void Update()
 	{
 
-		if (hp <= 0 || lung < 0)
-		{
-			gameObject.SetActive(false);
-			Turn.claimants.Remove(this);
-		}
+		//if (hp <= 0 || lung < 0)
+		//{
+		//	gameObject.SetActive(false);
+		//	Turn.claimants.Remove(this);
+		//}
 
 	}
 }
